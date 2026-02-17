4fe01389133e22eea3c4855a9dff63cd45dd0eb8
Chuy-pro <xdchuy04@gmail.com>
2026-02-17 15:18:17 -0600

    se agrego la paginacion al catalago al visualizar los productos en el welcome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -15,8 +15,36 @@
                     @endif
 
                     <h4>¡Hola, {{ Auth::user()->name }} !</h4>
+                    <p class="mb-4">Aquí se muestra el contenido protegido para usuarios autenticados.</p>
 
-                    Aquí se muestra el contenido protegido para usuarios autenticados.
+                    <h5 class="mt-4 mb-3">Productos Disponibles</h5>
+                    
+                    @if($products->count() > 0)
+                        <div class="row">
+                            @foreach($products as $product)
+                                <div class="col-md-4 mb-4">
+                                    <div class="card h-100">
+                                        @if($product->hasImage())
+                                            <img src="{{ $product->image_url }}" class="card-img-top" alt="{{ $product->name }}" style="height: 200px; object-fit: cover;">
+                                        @else
+                                            <div class="card-img-top bg-secondary d-flex align-items-center justify-content-center" style="height: 200px;">
+                                                <span class="text-white">Sin imagen</span>
+                                            </div>
+                                        @endif
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $product->name }}</h5>
+                                            <p class="card-text">{{ $product->description }}</p>
+                                            <p class="card-text"><strong>Precio: ${{ number_format($product->price, 2) }}</strong></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    @else
+                        <div class="alert alert-info">
+                            No hay productos disponibles en este momento.
+                        </div>
+                    @endif
                     
                 </div>
             </div>
